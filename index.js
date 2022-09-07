1ab03cb030bd48251b282f396af71f138ba0e6cc
DFTGardenHose <111650824+DFTGardenHose@users.noreply.github.com>
2022-09-07 16:54:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var edSoundClip = new Audio("sounds/ed-snake.m4a");
+var obSoundClip = new Audio("sounds/OB-DWM.m4a");
+
+
+$(".ob-pic").on("click", function() {
+  obSoundClip.play();
+});
+
+
+$(".ed-pic").on("click", function() {
+  edSoundClip.play();
+});
