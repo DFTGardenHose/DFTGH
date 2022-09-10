173baf674f582c32df46e2eefe94ce532c655687
DFTGardenHose <111650824+DFTGardenHose@users.noreply.github.com>
2022-09-10 08:31:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var edSoundClip = new Audio("sounds/ed-snake.m4a");
 var obSoundClip = new Audio("sounds/OB-DWM.m4a");
 
 $(".ob-pic").on("click", function() {
+  $(".ob-pic").toggleClass("spinOb");
   obSoundClip.play();
 });
 
 $(".ed-pic").on("click", function() {
+  $(".ed-pic").toggleClass("spinEd");
   edSoundClip.play();
 });
 
 $(".eric-pic").on("click", function() {
-  $(".eric-pic").addClass("spin");
+  $(".eric-pic").toggleClass("spin");
 });
