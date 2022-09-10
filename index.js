dbf9d83d6678bb9ed0268576d8c4b7739b8fa11b
DFTGardenHose <111650824+DFTGardenHose@users.noreply.github.com>
2022-09-10 08:11:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var edSoundClip = new Audio("sounds/ed-snake.m4a");
 var obSoundClip = new Audio("sounds/OB-DWM.m4a");
 
-
 $(".ob-pic").on("click", function() {
   obSoundClip.play();
 });
 
-
 $(".ed-pic").on("click", function() {
   edSoundClip.play();
+});
+
+$(".eric-pic").on("click", function() {
+  $(".eric-pic").addClass("spin");
 });
