78efb004af369c870094925a443788b7aee20303
DFTGardenHose <111650824+DFTGardenHose@users.noreply.github.com>
2022-12-31 16:08:24 -0500

    updated soundclips

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ var edSnakeSoundClip = new Audio("sounds/ed-snake.m4a");
 var edSnoreSoundClip = new Audio("sounds/ed-snore.m4a");
 var edTravelSoundClip = new Audio("sounds/ed-travel.m4a");
 var edKingSoundClip = new Audio("sounds/ed-king.m4a");
+var edGiselleSoundClip = new Audio("sounds/ed-giselle.m4a");
+var edSantaSoundClip = new Audio("sounds/ed-santa.m4a");
+var edToesSoundClip = new Audio("sounds/ed-toes.m4a");
 var obBoyDadSoundClip = new Audio("sounds/OB-boydad.m4a");
 var obTesticsSoundClip = new Audio("sounds/OB-testics.m4a");
 var obSwordsSoundClip = new Audio("sounds/OB-swords.m4a");
 var obVacationSoundClip = new Audio("sounds/OB-vacation.m4a");
 var obDWMSoundClip = new Audio("sounds/OB-DWM.m4a");
 var obRawdogSoundClip = new Audio("sounds/OB-rawdog.m4a")
+var obBearSoundClip = new Audio("sounds/OB-bear.m4a");
+var obSockieSoundClip = new Audio("sounds/OB-sockie.m4a");
 
 
 var ericSoundClip = new Audio("sounds/eric.m4a");
@@ -19,6 +24,7 @@ var ericSoundClip = new Audio("sounds/eric.m4a");
 
 $(".ed-pic").on("click", function() {
    $(".ed-pic").toggleClass("spinEd");
+   edUrinalSoundClip.play();
 });
 
 $("#ed-girldad").on("click", function() {
@@ -46,10 +52,21 @@ $("#ed-king").on("click", function() {
   edKingSoundClip.play();
 });
 
+$("#ed-giselle").on("click", function() {
+  $(".ed-pic").toggleClass("spinEdGiselle");
+  edGiselleSoundClip.play();
+});
+
+$("#ed-santa").on("click", function() {
+  $(".ed-pic").toggleClass("spinEdSanta");
+  edSantaSoundClip.play();
+});
+
 // OB Audio
 
 $(".ob-pic").on("click", function() {
   $(".ob-pic").toggleClass("spinOb");
+  obTesticsSoundClip.play();
 });
 
 
@@ -77,6 +94,16 @@ $("#ob-dwm").on("click", function() {
 $("#ob-rawdog").on("click", function() {
   $(".ob-pic").toggleClass("spinObRawdog");
   obRawdogSoundClip.play();
+});
+
+$("#ob-bear").on("click", function() {
+  $(".ob-pic").toggleClass("spinObBear");
+  obBearSoundClip.play();
+});
+
+$("#ob-sockie").on("click", function() {
+  $(".ob-pic").toggleClass("spinObSockie");
+  obSockieSoundClip.play();
 });
 
 // Eric Audio
